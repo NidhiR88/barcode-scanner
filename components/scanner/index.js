1a72ffa21ba64083e60cf5b7603a9265a3cbdf9d
Nidhi <54665514+NidhiR88@users.noreply.github.com>
2021-04-08 19:17:35 +0530

    added quagga js

---
+++ components/scanner/index.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import Quagga from 'quagga';
+
+class ScannerComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { barcodes: [] };
+    // this.onDetect = this.onDetect.bind(this);
+  }
+
+  componentDidMount() {
+    this.initializeScanning();
+  }
+
+  initializeScanning() {
+    Quagga.init(
+      {
+        constraints: {
+          facingMode: 'environment',
+        },
+        debug: {
+          drawBoundingBox: true,
+          drawScanLine: true,
+        },
+        locator: {
+          patchSize: 'Large',
+        },
+        decoder: {
+          readers: [
+            'code_128_reader',
+            // 'ean_8_reader',
+            // 'code_39_reader',
+            // 'code_39_vin_reader',
+            // 'codeabar_reader',
+          ],
+        },
+        locate: true,
+      },
+      function (err) {
+        if (err) {
+          return console.log('error code');
+        }
+        Quagga.start();
+      }
+    );
+    Quagga.onProcessed(this.onProcessed);
+  }
+
+  onDetect(result) {
+    debugger;
+    console.log(result.codeResult.code);
+    //check if valid result
+    if (result.codeResult.code) {
+      var scannedCode = result.codeResult.code;
+      this.setState({ barcodes: scannedCode }, () => {
+        Quagga.stop();
+      });
+    }
+  }
+
+  onProcessed = (result) => {
+    var drawingCtx = Quagga.canvas.ctx.overlay,
+      drawingCanvas = Quagga.canvas.dom.overlay;
+    if (result) {
+      if (result.boxes) {
+        drawingCtx.clearRect(
+          0,
+          0,
+          parseInt(drawingCanvas.getAttribute('width')),
+          parseInt(drawingCanvas.getAttribute('height'))
+        );
+        result.boxes
+          .filter(function (box) {
+            return box !== result.box;
+          })
+          .forEach(function (box) {
+            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+              color: 'green',
+              lineWidth: 2,
+            });
+          });
+      }
+
+      if (result.box) {
+        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+          color: '#00F',
+          lineWidth: 2,
+        });
+      }
+
+      if (result.codeResult && result.codeResult.code) {
+        Quagga.ImageDebug.drawPath(
+          result.line,
+          { x: 'x', y: 'y' },
+          drawingCtx,
+          { color: 'red', lineWidth: 3 }
+        );
+        Quagga.onDetected(this.onDetect);
+      }
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div id="interactive" className="viewport">
+          <video
+            className="videoInsert"
+            autoPlay={true}
+            preload="auto"
+            src=""
+            muted={true}
+            playsInline={true}
+          ></video>
+          <canvas
+            className="drawingBuffer videoInsert"
+            // styles={{ top: '50px', left: '50px', position: 'absolute' }}
+          ></canvas>
+        </div>
+        {this.state.barcodes.length !== 0 && (
+          <div>
+            <h1>Code:</h1>
+            {this.state.barcodes && this.state.barcodes.codeResult}
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export { ScannerComponent };
